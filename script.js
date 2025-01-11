8d586b1e90ab6fbdc1b4fb72600c66112a97a235
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2025-01-11 17:03:29 -0300

    adicionando comentarios ao codigo

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+//variaveis para a barra de input e o botão do html
 const input = document.querySelector(".input_task")
 const button = document.querySelector(".button_add_task")
 
+//variavél que armazena a lista que sera inserida no na ul do html referencia função showList()
 const finalList = document.querySelector(".list_task")
 
+//variavél que recebera as tarefas
 let taskList = []
 
-
+//função para adicionar as tarefas, inseri os item na list com o value e a condição de comcluida false
 function addTask(){
     taskList.push({
         tarefa: input.value,
@@ -18,6 +21,8 @@ function addTask(){
 
 }
 
+
+//função qu mostra a lista usando innerhtml adiciona o codigo no html, também adiciona os itens ao local storage
 function showTaskList(){
     let newLi = ""
 
@@ -38,12 +43,14 @@ function showTaskList(){
     
 }
 
+//função para deletar uma tarefa 
 function deleteTask (index) {
     taskList.splice(index, 1)
 
     showTaskList()
 }
 
+//função indentifica uma tarefa concluida para mudar o estilo, mudando o objeto completed da taskLits de false para true
 function completedTask (index) {
     taskList[index].completed = !taskList[index].completed 
 
@@ -51,6 +58,7 @@ function completedTask (index) {
 
 }
 
+// função para que as tarefas que foram adicionadas no local storage se apareçam inicalmente no projeto
 function refreshTask(){
     const localStorageTask = localStorage.getItem("lista")
 
@@ -61,4 +69,6 @@ function refreshTask(){
 }
 
 refreshTask()
+
+//adiciona um evento a variavel button, evento sendo o click e executa a função addTask
 button.addEventListener("click", addTask)
